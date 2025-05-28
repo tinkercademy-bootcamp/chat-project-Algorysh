ec85911345e19c99b96a1cef09087a4630372ca6
Rishika Garg <user@ip-172-31-18-95.ap-south-1.compute.internal>
2025-05-28 20:45:25 +0000

    Bug- Remove endline in the argument for check_error

--- exercise-3/src/tcp_echo_server.cc
+++ exercise-3/src/tcp_echo_server.cc
@@ -15,14 +15,14 @@ void check_error(bool test, std::string error_message) {
 
 int create_socket() {
   int my_sock = socket(AF_INET, SOCK_STREAM, 0);
-  check_error(my_sock < 0, "Socket creation error\n");
+  check_error(my_sock < 0, "Socket creation error");
   return my_sock;
 }
 
 bool set_socket_options(int sock, int opt) {
   auto err_code = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                  sizeof(opt));
-  check_error(err_code < 0, "setsockopt() error\n");
+  check_error(err_code < 0, "setsockopt() error");
   return true;
 }
 
@@ -36,12 +36,12 @@ sockaddr_in create_address(int port) {
 
 void bind_address_to_socket(int sock, sockaddr_in &address) {
   auto err_code = bind(sock, (sockaddr *)&address, sizeof(address));
-  check_error(err_code < 0, "bind failed\n");
+  check_error(err_code < 0, "bind failed");
 }
 
 void listen_on_socket(int sock) {
   auto err_code = listen(sock, 3);
-  check_error(err_code < 0, "listen failed\n");
+  check_error(err_code < 0, "listen failed");
 }
 
 void start_listening_on_socket(int my_socket, sockaddr_in &address) {
@@ -55,7 +55,7 @@ void start_listening_on_socket(int my_socket, sockaddr_in &address) {
 void handle_accept(int client_socket) {
   char buffer[kBufferSize] = {0};
   ssize_t valread = read(client_socket, buffer, kBufferSize);
-  check_error(valread < 0, "Read error on client socket" + std::to_string(client_socket) + "\n");
+  check_error(valread < 0, "Read error on client socket" + std::to_string(client_socket));
   if (valread > 0) {
     std::cout << "Received: " << buffer << "\n";
     send(client_socket, buffer, valread, 0);
